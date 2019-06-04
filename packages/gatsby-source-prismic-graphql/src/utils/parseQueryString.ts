310e987757446dfeb8da8a987280c0cfdb3ee4a6
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2019-06-04 16:37:59 +0000

    Update parseQueryString.ts

--- packages/gatsby-source-prismic-graphql/src/utils/parseQueryString.ts
+++ packages/gatsby-source-prismic-graphql/src/utils/parseQueryString.ts
@@ -1,8 +1,8 @@
 export function parseQueryString(qs: string = '', delimiter: string = '&'): Map<string, string> {
   return new Map(
     qs.split(delimiter).map(item => {
-      const [key, value] = item.split('=').map(part => decodeURIComponent(part.trim()));
-      return [key, value] as [string, string];
+      const [key, ...value] = item.split('=').map(part => decodeURIComponent(part.trim()));
+      return [key, value.join('=')] as [string, string];
     })
   );
 }
